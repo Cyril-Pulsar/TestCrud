d7cae6b34858b78322881231a400ff06faa428b6
Cyril-Pulsar <cyril.verdad@pulsar-sd.com>
2023-10-26 11:32:08 +0800

    added crud functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { SharedService } from './shared.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'AngularFireCrud';
+  constructor(private service: SharedService) {}
+  notes: any = [];
+  refreshNotes() {
+    this.service.getNotes().subscribe((res) => {
+      this.notes = res;
+    });
+  }
+
+  addNotes(newNotes: string) {
+    this.service.addNote(newNotes);
+  }
+
+  deleteNotes(id: string) {
+    this.service.deleteNote(id);
+  }
+
+  ngOnInit() {
+    this.refreshNotes();
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'angularcrud-6e28c',
+    appId: '1:753004342792:web:1d56f4318f92f4c9d02bbd',
+    storageBucket: 'angularcrud-6e28c.appspot.com',
+    apiKey: 'AIzaSyAwDG19tI11kp_4ZX5Vdiun59w1DTPRHYY',
+    authDomain: 'angularcrud-6e28c.firebaseapp.com',
+    messagingSenderId: '753004342792',
+  },
+};
